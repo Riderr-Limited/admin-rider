0ef83becb85c384101587c5536751258702ab36c
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2026-02-04 14:45:37 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,15 @@ export default function Home() {
   return (
     <div>
       <Link href="/login">Go to login page</Link>
+      <p>
+        Kindly think, check the app,analyse and implement any possible thing we
+        will need as admin
+      </p>
+      <p>You are to provide an adequete repsesntation</p>
+      <p>
+        You are to design all pages, states, and any other thing we will need
+      </p>
+      <p>YOUR MAIN FOCUS AND GOAL SHOULD BE STYILING</p>
     </div>
   );
 }
