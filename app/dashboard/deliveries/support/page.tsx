bcaa51445fcaf2afe83b82af514fcc2f3a65159c
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2026-02-04 14:45:43 +0100

    Create page.tsx

---
+++ app/dashboard/deliveries/support/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      the support should have our live chats, and the messages drop from the
+      from
+    </div>
+  );
+};
+
+export default page;
